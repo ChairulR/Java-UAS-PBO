ace25225fdfd8522fd6aa64b5cbfe5f7d4029f3a
ChairulR <mchairulrizki178@gmail.com>
2024-11-19 19:56:33 +0700

    Chairul 1

--- src/managementsystem/Menu.java
+++ src/managementsystem/Menu.java
@@ -468,6 +468,7 @@ public int z=0;
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        new updateBuyer().setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
